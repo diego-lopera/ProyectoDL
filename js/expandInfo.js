8310c0df83dba37ceeac17d475d973680b081dbf
Diego Aleis Lopera <diegoloperam1503@gmail.com>
2022-03-18 19:26:26 -0500

    fillStore Ok

--- js/expandInfo.js
+++ js/expandInfo.js
@@ -1,11 +1,12 @@
-export function expandInfo() {
+export function expandInfo(event) {
     
     let product ={}
     if(event.target.classList.contains("btn")){
         product={
             img:event.target.parentElement.querySelector("img").src,
             name:event.target.parentElement.querySelector("h4").textContent,
-            
+            price:event.target.parentElement.querySelector("h5").textContent,
+            description:event.target.parentElement.querySelector("h6").textContent,
         }
         console.log(product)
 
@@ -13,6 +14,11 @@ export function expandInfo() {
         img.src=product.img
         let name=document.getElementById("nameInfo")
         name.textContent=product.name
+        let price=document.getElementById("priceInfo")
+        price.textContent=product.price
+        let description=document.getElementById("descriptionInfo")
+        description.textContent=product.description
+        
     }
 
 }
--- js/storeFill.js
+++ js/storeFill.js
@@ -3,16 +3,16 @@ export function printStore() {
     // Array of Objects
 
 let products = [
-    {name:"Toyota 4RUNNER",price:350000000,image:'../img/imgStoreProducts/4runner.png',description:"Camioneta Toyota 4RUNNER 2023"},
-    {name:"Toyota Prado TX",price:150000000,image:'../img/imgStoreProducts/PradoTX.png',description:"Camioneta Toyota Prado TX 2023"},
-    {name:"Toyota Prado TXL",price:180000000,image:'../img/imgStoreProducts/PradoTXL.png',description:"Camioneta Toyota Prado TXL 2023"},
-    {name:"Toyota Hilux",price:90000000,image:'../img/imgStoreProducts/HILUX.png',description:"Pick up Toyota Hilux"},
-    {name:"Toyota Fortuner",price:250000000,image:'../img/imgStoreProducts/Fortuner.png',description:"Camioneta Toyota Fortuner"},
-    {name:"Toyota Land Cruiser 300",price:430000000,image:'../img/imgStoreProducts/LandCruiser300.png',description:"Camioneta Toyota Land Cruiser 300"},
-    {name:"Toyota Corolla",price:80000000,image:'../img/imgStoreProducts/Corolla.png',description:"Automovil Toyota Corolla"},
-    {name:"Toyota Yaris Crosss",price:120000000,image:'../img/imgStoreProducts/YarisCross.png',description:"Camioneta Toyota Yaris Cross"},
-    {name:"Toyota Corolla Cross",price:150000000,image:'../img/imgStoreProducts/COROLLACROSS.png',description:"Camioneta Toyota Corolla Cross"},
-    {name:"Toyota Rav4",price:310000000,image:'../img/imgStoreProducts/RAV4.png',description:"Camioneta Toyota Rav4"},
+    {name:"Toyota 4RUNNER",price:"350000000",image:'../img/imgStoreProducts/4runner.png',description:"Camioneta Toyota 4RUNNER 2023"},
+    {name:"Toyota Prado TX",price:"150000000",image:'../img/imgStoreProducts/PradoTX.png',description:"Camioneta Toyota Prado TX 2023"},
+    {name:"Toyota Prado TXL",price:"180000000",image:'../img/imgStoreProducts/PradoTXL.png',description:"Camioneta Toyota Prado TXL 2023"},
+    {name:"Toyota Hilux",price:"90000000",image:'../img/imgStoreProducts/HILUX.png',description:"Pick up Toyota Hilux"},
+    {name:"Toyota Fortuner",price:"250000000",image:'../img/imgStoreProducts/Fortuner.png',description:"Camioneta Toyota Fortuner"},
+    {name:"Toyota Land Cruiser 300",price:"430000000",image:'../img/imgStoreProducts/LandCruiser300.png',description:"Camioneta Toyota Land Cruiser 300"},
+    {name:"Toyota Corolla",price:"80000000",image:'../img/imgStoreProducts/Corolla.png',description:"Automovil Toyota Corolla"},
+    {name:"Toyota Yaris Crosss",price:"120000000",image:'../img/imgStoreProducts/YarisCross.png',description:"Camioneta Toyota Yaris Cross"},
+    {name:"Toyota Corolla Cross",price:"150000000",image:'../img/imgStoreProducts/COROLLACROSS.png',description:"Camioneta Toyota Corolla Cross"},
+    {name:"Toyota Rav4",price:"310000000",image:'../img/imgStoreProducts/RAV4.png',description:"Camioneta Toyota Rav4"},
 ]
 
 // Create Reference to fatherComponente
@@ -45,6 +45,16 @@ products.forEach(function(product){
     let title=document.createElement("h4")
     title.classList.add("text-center")
     title.textContent=product.name
+
+    let price=document.createElement("h5")
+    price.classList.add("text-center")
+    price.textContent=product.price
+    price.style.display='none'
+
+    let description=document.createElement("h6")
+    description.classList.add("text-center")
+    description.textContent=product.description
+    description.style.display='none'
     
 
     //
@@ -59,6 +69,8 @@ products.forEach(function(product){
 
     card.appendChild(image)
     card.appendChild(title)
+    card.appendChild(price)
+    card.appendChild(description)
     card.appendChild(button)
 
     column.appendChild(card)
